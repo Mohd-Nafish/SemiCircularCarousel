b7aa2a4cab9e5db1d0255244117a3bdc0dc4adbf
Mohd Nafish <83894846+Mohd-Nafish@users.noreply.github.com>
2026-06-04 19:31:17 +0530

    Initial Commit

---
+++ SemiCircularCarousel/SemiCircularCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  SemiCircularCarouselApp.swift
+//  SemiCircularCarousel
+//
+//  Created by Mohd Nafishuddin on 04/06/26.
+//
+
+import SwiftUI
+
+@main
+struct SemiCircularCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
